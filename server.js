aa53042a8cb0a2c20f25613daa73239125d0bf6b
MChandraR <2201020103@student.umrah.ac.id>
2024-09-20 19:00:54 +0700

    Project upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const createServer = require('http').createServer;
+
+const app = express();
+const http = createServer(app, {});
+
+app.use(express.static("./public"));
+
+app.listen(3000, (e)=>{
+    console.log("listening to 3000 at http://localhost:3000");
+});
