4cc8d9994a4eebe5b4735b1ab094050dbfe7fde3
MChandraR <2201020103@student.umrah.ac.id>
2024-09-20 19:07:42 +0700

    Project upload

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const createServer = require('http').createServer;
+const path = require('path');
 
 const app = express();
 const http = createServer(app, {});
+let usersPath = path.join(process.cwd(), "./public");
 
-app.use(express.static("public"));
+
+app.use(express.static(usersPath));
 
 app.listen(3000, (e)=>{
     console.log("listening to 3000 at http://localhost:3000");
